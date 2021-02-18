3edcd3164fa995a401b1733cdad43a04bfc1a0c1
KevinHoang06 <75810373+KevinHoang06@users.noreply.github.com>
2021-02-18 16:57:50 -0500

    Drive Train

--- src/main/java/frc/robot/commands/AutoMove.java
+++ src/main/java/frc/robot/commands/AutoMove.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class AutoMove extends CommandBase {
   private final DriveSubsystem drivesubsystem;
