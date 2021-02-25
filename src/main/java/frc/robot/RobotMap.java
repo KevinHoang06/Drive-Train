f59096378fc173a47cf090c4bcfd62affd5fb552
KevinHoang06 <75810373+KevinHoang06@users.noreply.github.com>
2021-02-25 15:35:30 -0500

    Flipped Port Numbers

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,13 +1,14 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
 public class RobotMap {
     public static final int LEFT_FRONT_PORT = 3;
     public static final int RIGHT_FRONT_PORT = 4;
-    public static final int LEFT_BACK_PORT = 2;
-    public static final int RIGHT_BACK_PORT = 1;
+    public static final int LEFT_BACK_PORT = 1;
+    public static final int RIGHT_BACK_PORT = 2;
 
     public static WPI_TalonFX leftFrontMotor = new WPI_TaloxFX(LEFT_FRONT_PORT);
     public static WPI_TalonFX rightFrontMotor = new WPI_TaloxFX(RIGHT_FRONT_PORT);
